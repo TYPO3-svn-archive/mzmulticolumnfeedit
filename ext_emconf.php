c36507fb730ddf392cce1dda37be8f54b9f640fb
z-mark <z-mark@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-30 09:54:29 +0000

    TER Version 1.0.1b manual.pdf added manual.sxw added manual.txt added smal code cleaning
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mzmulticolumnfeedit/trunk@41618 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mzmulticolumnfeedit".
 #
-# Auto generated 30-12-2010 09:54
+# Auto generated 30-12-2010 10:51
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'multicolumn' => '2.1.2',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"3a3a";s:10:"README.txt";s:4:"ee2d";s:27:"class.ux_tx_multicolumn.php";s:4:"0765";s:31:"class.ux_tx_multicolumn_pi1.php";s:4:"4fb6";s:12:"ext_icon.gif";s:4:"e000";s:17:"ext_localconf.php";s:4:"b068";s:19:"doc/wizard_form.dat";s:4:"1b52";s:20:"doc/wizard_form.html";s:4:"6ec2";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"3a3a";s:10:"README.txt";s:4:"ee2d";s:31:"class.ux_tx_multicolumn_pi1.php";s:4:"4fb6";s:12:"ext_icon.gif";s:4:"e000";s:17:"ext_localconf.php";s:4:"b068";s:14:"doc/manual.pdf";s:4:"26b7";s:14:"doc/manual.sxw";s:4:"91fa";s:14:"doc/manual.txt";s:4:"a366";s:19:"doc/wizard_form.dat";s:4:"1b52";s:20:"doc/wizard_form.html";s:4:"6ec2";}',
 	'suggests' => array(
 	),
 );
