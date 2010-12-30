e2eb9a4136bda36b762a3955ec99a97546205995
z-mark <z-mark@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-30 07:01:51 +0000

    import Kickstarter files
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mzmulticolumnfeedit/trunk@41613 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "mzmulticolumnfeedit".
+#
+# Auto generated 30-12-2010 07:28
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FE Edit for multicolumn',
+	'description' => 'Adds FrontendEdit support to multicolumn extension',
+	'category' => 'fe',
+	'author' => 'Markus Zuercher',
+	'author_email' => 'mzmulticolumnfeedit@markus-zuercher.ch',
+	'shy' => '',
+	'dependencies' => 'multicolumn,feedit',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'multicolumn' => '',
+			'feedit' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"3a3a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"1b52";s:20:"doc/wizard_form.html";s:4:"6ec2";}',
+);
+
+?>
